c5f772c76cd0e457c430435caa91fedc192df275
Durelli <rafael.durelli@ufla.br>
2022-01-28 10:41:31 -0300

    initial commit

---
+++ src/components/Input.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <p>{{ info }}</p>
+    Type: <input v-model="message" v-on:input="sendToBackEndAPI">
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Input",
+  data() {
+    return {
+      message: 'Type Here!',
+      info: null
+    }
+  },
+  methods: {
+    sendToBackEndAPI: function () {
+      const bodyToSend = {
+        name: "Erick" + Math.random(),
+        e_mail: "erick@ufla.com.br"
+      }
+      axios.post('https://60315fd1081a01001754701d.mockapi.io/cliente', bodyToSend).then(response => (this.info = response.data))
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
